aa83218fbf3478cf57cf6fb71602bab54238ebc0
Nathan <153100527+Akanaifu@users.noreply.github.com>
2024-10-26 21:08:23 +0200

    Add files via upload

--- hackaton 2024/js/classement.js
+++ hackaton 2024/js/classement.js
@@ -1,9 +1,9 @@
 "use strict";
 
 let matchs = [
-  { joueur1: "nathan", joueur2: "Lemaire", score: "21-2", temps: 8 },
-  { joueur1: "Argaba", joueur2: "xxxxx", score: "24-7", temps: 5 },
-  { joueur1: "William", joueur2: "yyyy", score: "17-0", temps: 3 },
+  { joueur1: "Nathan", joueur2: "Lemaire", score: "21-2", temps: 8 },
+  { joueur1: "Yves", joueur2: "Delvigne", score: "24-7", temps: 5 },
+  { joueur1: "Jonathan", joueur2: "Noel", score: "17-0", temps: 3 },
   // Add more matches as needed...
 ];
 
@@ -25,4 +25,25 @@ function createTbody(liste) {
         `;
   }
   tbody.innerHTML = ajout;
+}
+
+function tri(colonne) {
+  const nomDeColonne = colonne.id;
+  matchs.sort((a, b) => {
+    if (nomDeColonne === "joueur1" || nomDeColonne === "joueur2") {
+      return a[nomDeColonne]
+        .toLowerCase()
+        .localeCompare(b[nomDeColonne].toLowerCase());
+    }
+    if (nomDeColonne === "score") {
+      const [scoreA] = a.score.split("-").map(Number);
+      const [scoreB] = b.score.split("-").map(Number);
+      return scoreA - scoreB;
+    }
+    if (nomDeColonne === "temps") {
+      return a.temps - b.temps;
+    }
+    return 0;
+  });
+  createTbody(matchs);
 }
